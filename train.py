6d898b20cf881f1752ef253ce68a800f35c98c02
Luke <1483586698@qq.com>
2018-11-21 11:40:25 +0800

    sub perfect

--- train.py
+++ train.py
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 np.random.seed(1337)
 rdm = np.random.RandomState(1)
 
-dataset_size = 300
+dataset_size = 600 
 
 X = rdm.uniform(1, 300, (dataset_size, 2))
 # print(type(X))
 
 np.random.shuffle(X)
 
-Y = [(x1 + x2) for (x1, x2) in X]
+Y = [(x1 - x2) for (x1, x2) in X]
 #print(Y)
 
 #plt.scatter(X,Y)
 #plt.show()
-X_train,Y_train = X[:256],Y[:256]
+X_train,Y_train = X[:512],Y[:512]
 print(X_train)
 print(Y_train)
 
@@ -31,17 +31,16 @@ print(Y_train)
 model = Sequential()
 model.add(Dense(4, activation='relu', input_dim=2))
 model.add(Dense(8, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(4, activation='relu'))
+#model.add(Dense(4, activation='relu'))
+#model.add(Dense(4, activation='relu'))
 model.add(Dense(2, activation='relu'))
 model.add(Dense(1))
  
 #choose loss function and optimizing method
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.SGD(lr=0.000001))
+model.compile(loss='mean_squared_error', optimizer=keras.optimizers.SGD(lr=0.000001, momentum=0.9))
  
 print("Training.....")
-model.fit(X_train, Y_train, epochs=2000, batch_size=32)
+model.fit(X_train, Y_train, epochs=20000, batch_size=64)
 
 '''
 for step in range(301):
@@ -51,7 +50,7 @@ for step in range(301):
 '''
 print ("Testing.....")
 
-X_test,Y_test = X[256:],Y[256:]
+X_test,Y_test = X[512:],Y[512:]
 cost = model.evaluate(X_test,Y_test,batch_size=40)
 print ("test cost:",cost)
 W,b = model.layers[0].get_weights()
