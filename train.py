f4081a8a4d38a869773333ddf2f617e1871d72e4
efun100 <1483586698@qq.com>
2018-11-24 13:14:40 +0800

    mean_absolute_percentage_error

--- train.py
+++ train.py
@@ -12,9 +12,9 @@ from keras.layers.advanced_activations import LeakyReLU
 np.random.seed(1337)
 rdm = np.random.RandomState(1)
 
-dataset_size = 600 
+dataset_size = 1100
 
-X = rdm.uniform(1, 30, (dataset_size, 2))
+X = rdm.uniform(1, 100, (dataset_size, 2))
 # print(type(X))
 
 np.random.shuffle(X)
@@ -24,7 +24,7 @@ Y = [(x1 * x2) for (x1, x2) in X]
 
 #plt.scatter(X,Y)
 #plt.show()
-X_train,Y_train = X[:512],Y[:512]
+X_train,Y_train = X[:1024],Y[:1024]
 print(X_train)
 print(Y_train)
 
@@ -45,7 +45,7 @@ model.add(LeakyReLU())
 model.add(Dense(1))
  
 #choose loss function and optimizing method
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.SGD(lr=0.0000001, momentum=0.9))
+model.compile(loss='mean_absolute_percentage_error', optimizer=keras.optimizers.SGD(lr=0.000001, momentum=0.9))
  
 print("Training.....")
 model.fit(X_train, Y_train, epochs=100000, batch_size=64)
@@ -58,7 +58,7 @@ for step in range(301):
 '''
 print ("Testing.....")
 
-X_test,Y_test = X[512:],Y[512:]
+X_test,Y_test = X[1024:],Y[1024:]
 cost = model.evaluate(X_test,Y_test,batch_size=40)
 print ("test cost:",cost)
 W,b = model.layers[0].get_weights()
